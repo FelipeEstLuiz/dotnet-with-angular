1bacc44d229cc45b49e71d11f8682b8bed1c0021
Felipe Estevam Luiz <fel13.luiz@gmail.com>
2025-04-16 16:34:47 -0300

    incluido navegacao

--- src/Application.Client/src/app/app.component.ts
+++ src/Application.Client/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
 import { HomeComponent } from './home/home.component';
 import { AppLoadingComponent } from './shared/loading/loading.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavComponent, HomeComponent, AppLoadingComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent, AppLoadingComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
